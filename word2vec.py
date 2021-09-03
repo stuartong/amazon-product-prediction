6890238d5f5c20ec3653e4af04bbb37c183503bb
mgendia <moutazgendia@gmail.com>
2021-09-03 11:26:28 +0400

    updated generate_desne_function to match the new verssion of gensim 4.0

--- word2vec.py
+++ word2vec.py
@@ -13,21 +13,21 @@ def get_pretrained_model(target_model= 'glove-wiki-gigaword-300' ):
         pretrained KeyedVector
     """
     import gensim.downloader as api
-    return gensim.models.KeyedVectors.load(api.load(target_model))
+    return api.load(target_model)
 
-def generate_dense_features(tokenized_texts, model_vectors= None, use_mean= True):
+def generate_dense_features(tokenized_texts, model= None, use_mean= True):
     """This function takes tokenized_texts in list format and a pretrained word2vec model
     and returns an array of either a vector of word embeddings or the mean of that vector
     depending on the users choice
     
     Args:
         tokenized_texts (list): list of strings to match agains the pretrained word2vec model
-        model_vectors (matrix): matrix of words and their respective word embedding vectors
+        model (matrix): matrix of words and their respective word embedding vectors
         use_mean (bool, optional): True to get the mean of the vector, False to return full vector. Default to 'True'
 
     Returns:
         vector: vector/ or average of the vector of embedding weights for every word"""
-    if model_vectors == None:
+    if model == None:
         print("""Must chose a pretrained model
               Try:
               - gensim.models.KeyedVectors.load('assets/wikipedia.100.word-vecs.kv',
@@ -36,19 +36,20 @@ def generate_dense_features(tokenized_texts, model_vectors= None, use_mean= True
               or type print(list(gensim.downloader.info()['models'].keys())) to get all available list in gensim
               """)
     else:
+        cached_vocab= model.key_to_index.keys()
         target_list= []
         for item in tqdm(tokenized_texts):
-            words= [word for word in item if word in model_vectors.vocab]
+            words= [word for word in item if model.index_to_key]
         if len(words) > 0:
             if use_mean == True:
                 #get the mean of the word2vec of every word in tokenized_text
-                feature= np.mean(model_vectors[words], axis= 0)
+                feature= np.mean(model[words], axis= 0)
                 target_list.append(feature)
             else:
                 #just append the full word2vec
-                target_list.append([model_vectors[word] for word in words])
+                target_list.append([model[word] for word in words])
         else:
             #just append zeros with the same vector dimension
-            target_list.append(model_vectors.vector_size)
+            target_list.append(model.vector_size)
     return np.array(target_list)
             
