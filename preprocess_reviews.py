5aa1d4e78b579ad01f801bc127c8d8e09414cae3
mgendia <moutazgendia@gmail.com>
2021-09-10 11:04:23 +0400

    updating commented text

--- preprocess_reviews.py
+++ preprocess_reviews.py
@@ -12,6 +12,8 @@ def preprocess_reviews(review_df_path):
     #Step2 : cleaning the text in 'reviewText', 'summary' columns
     for col in ['reviewText', 'summary']:
         review_df[col]= review_df[col].apply(lambda row: clean_review(row))
+        
+    ###NOTE_TO_SELF: NEED TO FIX THIS PART BELOW-- CHECK DATA TYPES TO CONCAT###
     #Step3 : merging 'reviewText', 'summary' columns into 1 and extracting alphanumeric values only
     # review_df = create_full_feature_review(review_df)
     return review_df
