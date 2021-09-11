5cba1fac4074b0bb23f81937a585783904cd1cd6
sheilavp <70035222+sheilavp@users.noreply.github.com>
2021-09-10 23:54:04 -0500

    altered the review_df STEP1  cleaning

--- get_product_success.py
+++ get_product_success.py
@@ -90,7 +90,7 @@ def get_product_success(cat_meta_url,cat_review_url):
     
     ### let us futher process the review_df alone
     #Step1 : cleaning the review_df from any duplicates in 'reviewText' column
-    #review_df = clean_review_column(review_df)
+    review_df = clean_review_column(review_df)
     #Step2 : cleaning the text in 'reviewText', 'summary' columns
     for col in ['reviewText', 'summary']:
         review_df[col]= review_df[col].apply(lambda row: clean_review(row))
