0dfc41a80077889e0c9fd1d86c47b059fa309717
sheilavp <70035222+sheilavp@users.noreply.github.com>
2021-09-10 21:15:30 -0500

    Update preprocess_review_data.py
    uncomment the first part and minor changes

--- preprocess_review_data.py
+++ preprocess_review_data.py
@@ -1,33 +1,40 @@
 import pandas as pd
 import numpy as np
 
-def clean_review_column(df):
-    """
-    clean_review_text_column function takes in a dataframe, dropping any reviewText duplicates
+import re
+import nltk
+import string
+
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords, wordnet
+
+# def clean_review_column(df):
+#     """
+#     clean_review_text_column function takes in a dataframe, dropping any reviewText duplicates
     
-    Args:
-        df (data_frame): review data_frame 
-    Returns:
-        df: cleaned version of the dataframe
-    """
+#     Args:
+#         df (data_frame): review data_frame 
+#     Returns:
+#         df: cleaned version of the dataframe
+#     """
     
-    #dropping any duplicates
-    print('Dropping any duplicates in the reviewText column')
-    df = df.drop_duplicates(subset=['reviewText'])
+#     #dropping any duplicates
+#     print('Dropping any duplicates in the reviewText column')
+#     df = df.drop_duplicates(subset=['reviewText'])
     
-    #filtering any unverified reviews out
-    # print('Let us filter the unverified reviews out')
-    # df = df[df['verified']==True]
-    return df
+#     #filtering any unverified reviews out
+#     # print('Let us filter the unverified reviews out')
+#     # df = df[df['verified']==True]
+#     return df
 
 def clean_review(sent):
     """
-    takes a string 
-    - remove punctuations
-    - lowercase all words
-    - tokenize
-    - lemmatize
-    - remove stopwords
+    takes in a string of text, and performs the following steps:
+    ######1. remove punctuations - ON HOLD
+    2. lowercase all words
+    3. tokenize
+    4. lemmatize
+    5. remove stopwords
     
     and returns a list of lemmatized words that are not in the NLTK english stopwords
     
@@ -37,23 +44,20 @@ def clean_review(sent):
         list: list of lemmatized version of the original text
     """
     
-    import re
-    from nltk.stem import WordNetLemmatizer
-    from nltk.corpus import stopwords
-    stop_words= stopwords.words('english')
-    lemmatizer= WordNetLemmatizer()
+    stop_words = stopwords.words('english')
+    lemmatizer = WordNetLemmatizer()
 
     if type(sent) == list:
-        text= ' '.join(sent)
-        extracted_words=  re.findall(r'(\w+|\d\'\d)', string=text)
-        filtered_words= [word.lower() for word in extracted_words if (word not in stop_words) & (len(word)>1)]
-        lemmatized_words= [lemmatizer.lemmatize(word) for word in filtered_words]
+        text = ' '.join(sent)
+        extracted_words =  re.findall(r'(\w+|\d\'\d)', string=text)
+        filtered_words = [word.lower() for word in extracted_words if (word not in stop_words) & (len(word)>1)]
+        lemmatized_words = [lemmatizer.lemmatize(word) for word in filtered_words]
         return lemmatized_words
     elif type(sent) == str:
         # print('dtype is string')
-        extracted_words=  re.findall(r"(\w+|\d\'\d)", string=sent) #FIX THIS, if want to capture 5'6" for example
-        filtered_words= [word.lower() for word in extracted_words if (word not in stop_words) & (len(word)>1)]
-        lemmatized_words= [lemmatizer.lemmatize(word) for word in filtered_words]
+        extracted_words =  re.findall(r"(\w+|\d\'\d)", string=sent) #FIX THIS, if want to capture 5'6" for example
+        filtered_words = [word.lower() for word in extracted_words if (word not in stop_words) & (len(word)>1)]
+        lemmatized_words = [lemmatizer.lemmatize(word) for word in filtered_words]
         return lemmatized_words
     
 def create_full_feature_review(df):
@@ -68,6 +72,6 @@ def create_full_feature_review(df):
         df (dataframe): updated dataframe with full_review_features column
     """
     
-    df['full_review_features']= (df['reviewText']+ df['summary']).map(pd.unique)
+    df['full_review_features'] = (df['reviewText']+ df['summary']).map(pd.unique)
     df.drop(columns= ['reviewText', 'summary'], inplace= True)
     return df
