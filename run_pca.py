8f96aacdcdade0e26cd44b47170fee3ac6916835
mgendia <moutazgendia@gmail.com>
2021-09-13 18:45:20 +0400

    updating pca file and params to run on 1D and return features arrays

--- run_pca.py
+++ run_pca.py
@@ -3,12 +3,14 @@ from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import numpy as np
 
-def run_pca(array):
-    target_arr= array.reshape(-1,1)
+def run_pca(df):
+    df= df[df["features"].map(len)> 50]
+    target_arr= np.hstack(df["features"].values).reshape(-1,304)
     import yaml
     with  open("params.yaml", "r") as file:
         params= yaml.safe_load(file)
         n_components= params["run_pca"]["n_components"]
+    
     #initializing StandardScaler
     scaler= StandardScaler()
     #scaling the array
@@ -16,6 +18,11 @@ def run_pca(array):
     #initializing pca and generating components
     pca_= PCA(n_components= int(n_components))
     principal_components= pca_.fit_transform(target_arr)
+    df1= pd.DataFrame(principal_components).apply(list, axis=1)
+    df.reset_index(inplace= True)
+    df["features"]= df1
+    explained_variance_ratio= pca_.explained_variance_ratio_
+    # print("pca explained variance ration= ", explained_variance_ratio)
+    print("total explained variance from {} PCAs = ".format(n_components), np.sum(explained_variance_ratio))
     #returning an array of concatenated components 
-    return np.array([component for component in principal_components])
-
+    return df
