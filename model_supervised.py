cb67543f7e134512218268b0eea96d6cb8a99b16
Stuart Ong <onstuart@gmail.com>
2021-09-17 11:31:14 +0800

    Updated params to handle base model for ada boost

--- model_supervised.py
+++ model_supervised.py
@@ -51,7 +51,8 @@ def run_model(df_path):
         SVC= SVC,
         GradientBoostingClassifier= GradientBoostingClassifier,
         MLPClassifier= MLPClassifier,
-        AdaBoostClassifier=AdaBoostClassifier
+        AdaBoostClassifier=AdaBoostClassifier,
+        DecisionTreeClassifier=DecisionTreeClassifier
         )
     
     import yaml
@@ -59,6 +60,8 @@ def run_model(df_path):
         param_file= yaml.safe_load(file)
         
     model_type= model_dict[param_file["supervised_model"]["model_type"]]
+    ada_base_model = param_file["supervised_model"]['ada_base_model']
+    ada_base_iter = param_file["supervised_model"]['ada_base_iter']
     params= param_file["supervised_model"]["params"] 
     scale= param_file["supervised_model"]["scale"]
     oversample= param_file["supervised_model"]["oversample"]
@@ -67,6 +70,10 @@ def run_model(df_path):
     n_jobs= param_file["supervised_model"]["n_jobs"]
     return_train_score= param_file["supervised_model"]["return_train_score"]
     split= param_file["supervised_model"]["split"]
+
+    if model_type == AdaBoostClassifier:
+        param_dict["base_estimator"]= [model_dict[ada_base_model](max_depth= i) for i in ada_base_iter]
+
    
     #import data
     df= pd.read_pickle(df_path+ "/products.pkl") 
