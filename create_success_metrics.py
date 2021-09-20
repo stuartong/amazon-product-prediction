95491fb3862c03929c5048bc9e5546f320046e21
mgendia <moutazgendia@gmail.com>
2021-09-20 13:03:41 +0400

    updated path structure to work on windows and mac

--- create_success_metrics.py
+++ create_success_metrics.py
@@ -2,14 +2,17 @@ import pandas as pd
 import numpy as np
 from create_success_metric_module import create_success_metric, check_score_rank
 import os
+from pathlib import Path
 
 def create_success_metrics(product_path, review_path):
     import yaml
     with open("params.yaml", "r") as file:
         params= yaml.safe_load(file)
     cutoff= params["success_metrics"]["cutoff"]
+    product_path= Path(product_path)
+    review_path= Path(review_path)
     #importing product_df, review_df
-    product_df, review_df= [pd.read_pickle(file) for file in [product_path + "/products.pkl",  review_path + "/reviews.pkl"]]
+    product_df, review_df= [pd.read_pickle(file) for file in [product_path / "products.pkl",  review_path / "reviews.pkl"]]
     product_df= create_success_metric(product_df, review_df, cutoff= cutoff)
     # fill NaN with 0 in df - i.e. no reviews
     product_df[['tot_stars','tot_reviews','avg_stars']] = product_df[['tot_stars','tot_reviews','avg_stars']].fillna(value=0)
@@ -24,19 +27,7 @@ if __name__ == "__main__":
     # parser.add_argument("cutoff", help= "cuttoff mark of number of points to consider a product good or bad")
     args= parser.parse_args()
     product_df= create_success_metrics(args.products_path, args.reviews_path)
-    if os.path.isdir("data"):
-        if os.path.isdir("data/metrics"):
-            product_df.to_pickle('data/metrics/products.pkl')
-        else:
-            parent_dir= "data"
-            directory= "metrics"
-            path= os.path.join(parent_dir, directory)
-            os.makedirs(path, exist_ok= True)
-            product_df.to_pickle('data/metrics/products.pkl')
-    else:
-        parent_dir= os.mkdir("data")
-        directory= "metrics"
-        path= os.path.join(parent_dir, directory)
-        os.makedirs(path)
-        product_df.to_pickle('data/metrics/products.pkl') 
+
+    Path("data/metrics").mkdir(parents= True, exist_ok= True)
+    product_df.to_pickle('data/metrics/products.pkl') 
     
